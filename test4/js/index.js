60ff814da2f5253f36a213db869e2a8eb620bcb9
Viktoria Shchypalkina <37249354+helavika@users.noreply.github.com>
2019-02-07 22:42:26 +0200

    Update index.js

--- test4/js/index.js
+++ test4/js/index.js
@@ -2,7 +2,7 @@
 
 var lang = [
 	{ 
-		language: "english",
+		language: 'english',
 		keyboard: {
 			1: 'qwertyuiop',
 			2: ']asdfghjkl;\'',
@@ -11,7 +11,7 @@ var lang = [
 	},
 
 	{ 
-		language: "русский",
+		language: 'русский',
 		keyboard: {
 			1: 'йцукенгшщзхъ',
 			2: 'фывапролджэ',
@@ -20,7 +20,7 @@ var lang = [
 	},
 
 	{ 
-		language: "українська",
+		language: 'українська',
 		keyboard: {
 			1: 'йцукенгшщзхї',
 			2: 'фівапролджє',
@@ -29,33 +29,46 @@ var lang = [
 	}
 ]
 
-function langChoice() {
-	var chooseLang = prompt(`Select the language: ${lang[0].language}, ${lang[1].language}, ${lang[2].language}`);
-	
-	switch (chooseLang) {
-		case '1':
-			var symbol = prompt(`Choose some`);
-			break;
-		case '2':
-			var symbol = prompt(`Впиши символ`);
-			break;
-		case '3': 
-			var symbol = prompt(`Будь що`)
+let symbol;
+
+let index;
+
+let i;
+
+var chooseLang = prompt(`Select the language: ${lang[0].language}, ${lang[1].language}, ${lang[2].language}`);
+
+switch (chooseLang) {
+
+	case '1':
+		symbol = prompt(`Choose some`);
+	break;
+
+	case '2':
+		symbol = prompt(`Впиши символ`);
+	break;
+
+	case '3': 
+		symbol = prompt(`Будь що`)
 	}
 
-	// function find(array, value) {
- //      for (var i=0; i<array.length; i++)
- //      if (array[i].indexOf(value) != -1) {return [i,array[i].indexOf(value)]  }
- //      return -1
-	// }
+function find (lang, symbol) {
 
-	alert(find(lang, symbol));
+	for (var i = 0; i < lang.length; i++) {
+
+		const obj = lang[i];
+
+		const keyboard = obj.keyboard;
+
+		const keys = Object.keys(keyboard);
+
+		keys.forEach((key, i) => {
+			const string = keyboard[key];
+
+			index = string.indexOf(symbol);
+		});
+
+		alert(`${index} - ${i}`) 
+	}
 }
-langChoice();
 
-
-
-
-
-
-
+find (lang, symbol)
